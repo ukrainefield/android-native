44e6ca33ce2dffab7bf53f7be0e0a99bd8f34f02
Jaap <33700526+GewoonJaap@users.noreply.github.com>
2022-03-11 17:57:06 +0100

    Bookmark helper

--- app/app/src/main/java/nl/gardensnakes/ukrainefield/MainActivity.kt
+++ app/app/src/main/java/nl/gardensnakes/ukrainefield/MainActivity.kt
@@ -2,8 +2,10 @@ package nl.gardensnakes.ukrainefield
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import nl.gardensnakes.ukrainefield.helper.BookmarkHelper
 
 class MainActivity : AppCompatActivity() {
     private lateinit var bottomNavigationbar: BottomNavigationView
