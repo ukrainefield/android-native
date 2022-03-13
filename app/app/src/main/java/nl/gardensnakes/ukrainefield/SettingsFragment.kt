e1a4330e01a34e74dba8c908c700686081a40bb7
Jaap <33700526+GewoonJaap@users.noreply.github.com>
2022-03-13 14:45:13 +0100

    Add links to settings

--- app/app/src/main/java/nl/gardensnakes/ukrainefield/SettingsFragment.kt
+++ app/app/src/main/java/nl/gardensnakes/ukrainefield/SettingsFragment.kt
@@ -1,11 +1,15 @@
 package nl.gardensnakes.ukrainefield
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.preference.*
+import androidx.core.content.ContextCompat
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.google.firebase.analytics.FirebaseAnalytics
 import nl.gardensnakes.ukrainefield.helper.BookmarkHelper
+import nl.gardensnakes.ukrainefield.helper.Consts
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
@@ -19,9 +23,41 @@ class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.settings, rootKey)
+        setupResetBookmarksClick()
+        setupNewsSourcesClick()
+        setupGithubReposClick()
+        setupTwitterClick()
+    }
+
+    private fun setupResetBookmarksClick() {
         findPreference<Preference>("reset_bookmarks")?.setOnPreferenceClickListener {
             context?.let { context -> BookmarkHelper().resetBookmarks(context) }
             true
         }
     }
+
+    private fun setupTwitterClick() {
+        findPreference<Preference>("twitter_link")?.setOnPreferenceClickListener {
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(Consts.TWITTER_LINK))
+            ContextCompat.startActivity(requireContext(), browserIntent, null)
+            true
+        }
+    }
+
+    private fun setupGithubReposClick() {
+        findPreference<Preference>("github_link")?.setOnPreferenceClickListener {
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(Consts.GITHUB_LINK))
+            ContextCompat.startActivity(requireContext(), browserIntent, null)
+            true
+        }
+    }
+
+    private fun setupNewsSourcesClick() {
+        findPreference<Preference>("news_sources_link")?.setOnPreferenceClickListener {
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(Consts.NEWSSOURCES_LINK))
+            ContextCompat.startActivity(requireContext(), browserIntent, null)
+            true
+        }
+    }
+
 }
---
+++ app/app/src/main/java/nl/gardensnakes/ukrainefield/helper/Consts.kt
@@ -0,0 +1,7 @@
+package nl.gardensnakes.ukrainefield.helper
+
+object Consts {
+    val NEWSSOURCES_LINK: String = "https://github.com/ukrainefield/data-fetcher/blob/main/consts.json"
+    val GITHUB_LINK: String = "https://github.com/ukrainefield"
+    val TWITTER_LINK: String = "https://twitter.com/fieldukraine"
+}
